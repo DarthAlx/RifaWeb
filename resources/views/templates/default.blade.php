e7a2b86325cb7a479094e05eb91a6a1b3696ee16
Alexis Morales <alx.morales@hadoukendev.com>
2017-12-19 21:27:02 -0600

    Navbar

---
+++ resources/views/templates/default.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="{{ url('css/bootstrap.min.css') }}" media="screen" />
+        <link rel="stylesheet" type="text/css" href="{{ url('css/materialize.css') }}" media="screen" />
+        <link rel="stylesheet" type="text/css" href="{{ url('css/font-awesome.min.css') }}" media="screen" />
+        <link rel="stylesheet" type="text/css" href="{{ url('css/style.css') }}" media="screen" />
+
+        <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+    <body>
+        <section class="navigation">
+            <div class="container">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+
+                  <div class="collapse navbar-collapse row" id="navbarSupportedContent">
+
+                    <ul class="navbar-nav col l4">
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">SORTEOS</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">¿CÓMO FUNCIONA?</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">NOSOTROS</a>
+                      </li>
+                    </ul>
+                    <div class="col l4 text-center">
+                        <a class="navbar-brand auto " href="#"><img src="{{url('img/Rifaweb2.png')}}" alt=""></a>
+                    </div>
+                    
+                    <ul class="navbar-nav justify-content-end col l4">
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">MI CUENTA</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">CARRITO</a>
+                      </li>
+                    </ul>
+                    
+                    
+                  </div>
+                </nav>
+            </div>
+        </section>
+        <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+        <script type="text/javascript" src="{{ url('js/materialize.min.js') }}"></script>
+        <script type="text/javascript" src="{{ url('js/popper.js') }}"></script>
+        <script type="text/javascript" src="{{ url('js/bootstrap.min.js') }}"></script>
+        <script type="text/javascript" src="{{ url('js/script.js') }}"></script>
+    </body>
+</html>
