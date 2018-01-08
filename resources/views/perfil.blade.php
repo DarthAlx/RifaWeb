fd34ec4c6bcd3a9b273ce7f6e2c471c59eb134d0
Alexis Morales <alx.morales@hadoukendev.com>
2018-01-08 09:24:32 -0600

    PR

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -1,1 +1,13 @@
-{{dd("Zona segura")}}
+@extends('templates.default')
+
+@section('pagecontent')
+<section class="perfil">
+    <div >        
+        <div class="social-login">
+            <div class="">
+                <h4 style="margin-bottom: 0;">MI CUENTA</h4>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
